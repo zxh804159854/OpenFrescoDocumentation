f8d5d2d72f5f3ed94d5bd9bea146fbb1d7edfed2
zxh804159854 <zxh804159854@163.com>
2026-04-28 01:16:59 +0800

    修改模板和初始测试

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -14,7 +14,7 @@ release = '0.0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['myst_parser'] #支持markdown
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +24,5 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme" #更改主题
 html_static_path = ['_static']
